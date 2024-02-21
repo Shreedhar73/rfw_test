9db98713a34dd2bfdd5801ffda65728d3932ea27
Shreedhar <shreedhar@Shreedhars-Laptop>
2024-02-21 15:24:29 +0545

    j

--- remote_widgets/encoder.dart
+++ remote_widgets/encoder.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:rfw/formats.dart';
 
 void main() {
-  final String counterApp1 = File('test_data.rfwtxt').readAsStringSync();
+  final String counterApp1 = File('a.rfwtxt').readAsStringSync();
   File('text_data.rfw')
       .writeAsBytesSync(encodeLibraryBlob(parseLibraryFile(counterApp1)));
 }
